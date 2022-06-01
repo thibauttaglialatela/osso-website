75611784144b7053efbecac97d26ad8a845a790c
thibauttaglialatela <thibauttaglialatela@gmail.com>
2022-06-01 16:55:46 +0200

    ajout de webpack encore + images + scss

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,9 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+#[Route('/', name: 'app_')]
+class DefaultController extends AbstractController
 {
-    #[Route('/', name: 'Home_index')]
+    #[Route('', name: 'index')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,10 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
 Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
+
+    .copyFiles({
+        from: './assets/images',
+    })
     // public path used by the web server to access the output path
     .setPublicPath('/build')
     // only needed for CDN's or sub-directory deploy
@@ -56,7 +60,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
