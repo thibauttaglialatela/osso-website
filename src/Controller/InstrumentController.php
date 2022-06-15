04d31f0f08db1855a2acde53bee790a4cb5b0e39
thibauttaglialatela <thibauttaglialatela@gmail.com>
2022-06-15 16:46:37 +0200

    ajout des cards instruments

---
+++ src/Controller/InstrumentController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\InstrumentRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class InstrumentController extends AbstractController
+{
+    #[Route('orchestra/instrument', name: 'orchestra_instrument')]
+    public function index(InstrumentRepository $instrumentRepository): Response
+    {
+        $instruments = $instrumentRepository->findAll();
+
+
+        return $this->render('instrument/index.html.twig', [
+            'instruments'=>$instruments,
+        ]);
+    }
+}
