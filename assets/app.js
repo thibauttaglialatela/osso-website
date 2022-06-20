5f1231b0be2b9e18f4f7b6b9ebd93fa85013a3c6
thibauttaglialatela <thibauttaglialatela@gmail.com>
2022-06-20 15:56:36 +0200

    installation du module datatables dans le projet

--- assets/app.js
+++ assets/app.js
@@ -1,15 +1,17 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
+const $ = require( 'jquery' );
+global.$ = global.jQuery = $;
 // any CSS you import will output into a single css file (app.scss in this case)
 import './styles/app.scss';
 
 // start the Stimulus application
 import 'bootstrap';
 
-console.log('Hello world !')
+import 'datatables.net-bs5';
+
+$(document).ready(function () {
+    $('#osso-repertory').DataTable();
+});
+
+
+
 
