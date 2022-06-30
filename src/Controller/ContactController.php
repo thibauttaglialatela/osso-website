edb423a7f7ac443e9aab77e4c7b30594e6a87397
thibauttaglialatela <thibauttaglialatela@gmail.com>
2022-06-30 13:43:50 +0200

    create ContactController

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+#[Route('/contact', name: 'contact_')]
+class ContactController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('contact/index.html.twig', [
+            'controller_name' => 'ContactController',
+        ]);
+    }
+}
--- src/DataFixtures/ContactFixtures.php
+++ src/DataFixtures/ContactFixtures.php
@@ -2,15 +2,24 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Contact;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class ContactFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+        for ($i = 0; $i < 5; $i++) {
+            $contact = new Contact();
+            $contact->setFullName($faker->name)
+                ->setEmail($faker->email)
+                ->setSubject("Demande n° " . ($i + 1))
+                ->setMessage($faker->realText);
+            $manager->persist($contact);
+        }
 
         $manager->flush();
     }
