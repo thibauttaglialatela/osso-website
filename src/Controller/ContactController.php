3660d05dd3f8aac8c715ec36bf5446a91c473ad3
thibauttaglialatela <thibauttaglialatela@gmail.com>
2022-06-30 16:36:13 +0200

    create form contact + ajout HTML sanitizer

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactType;
+use App\Repository\ContactRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 #[Route('/contact', name: 'contact_')]
 class ContactController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(Request $request, ContactRepository $contactRepository): Response
     {
-        return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contactRepository->add($contact, true);
+            return $this->redirectToRoute('contact_index');
+        }
+        return $this->renderForm('contact/index.html.twig', [
+            'form' => $form,
         ]);
     }
 }
---
+++ src/Form/ContactType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('fullName', TextType::class, [
+                'required'=>false,
+                'label'=>'Nom',
+                'label_attr'=>['class'=>'fs-3']
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'e-mail',
+                'label_attr'=>['class'=>'fs-3']
+            ])
+            ->add('subject', TextType::class, [
+                'required'=>false,
+                'label'=>'sujet du message',
+                'label_attr'=>['class'=>'fs-3']
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => 'Votre message',
+                'label_attr'=>['class'=>'fs-3'],
+            ])
+            ->add('save', SubmitType::class, [
+                'attr' => [
+                    'class'=>'btn btn-success mt-2'
+                ],
+                'label' => 'Envoyer votre message'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+            'sanitize_html' => true,
+        ]);
+    }
+}
