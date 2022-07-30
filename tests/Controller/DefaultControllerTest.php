3f1fd7b16576d551026bf31eaeeca4b49606cbc4
thibauttaglialatela <thibauttaglialatela@gmail.com>
2022-07-30 16:25:34 +0200

    connection à la partie admin du site par login OK

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -1,18 +1,17 @@
 <?php
 
-namespace App\Tests;
+namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testSomething(): void
+    public function testH1(): void
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/');
 
         $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'Hello World');
-        $crawler = $client->followRedirect();
+        $this->assertSelectorTextContains('h1', 'Bienvenue sur le site de l\'Orchestre Symphonique du Sud Ouest');
     }
 }
