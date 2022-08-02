f15875d7210f325794822c840e87b2359ee75b8b
thibauttaglialatela <thibauttaglialatela@gmail.com>
2022-08-02 15:19:09 +0200

    MAJ entity compositor avec ajout contrainte nom unique

--- src/Entity/Compositor.php
+++ src/Entity/Compositor.php
@@ -6,8 +6,11 @@ use App\Repository\CompositorRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CompositorRepository::class)]
+#[UniqueEntity('name')]
 class Compositor
 {
     #[ORM\Id]
@@ -15,7 +18,7 @@ class Compositor
     #[ORM\Column(type: 'integer')]
     private int $id;
 
-    #[ORM\Column(type: 'string', length: 255)]
+    #[ORM\Column(type: 'string', length: 255, unique: true)]
     private string $name;
 
     #[ORM\OneToMany(mappedBy: 'compositor', targetEntity: MusicalWork::class)]
--- src/Form/CompositorType.php
+++ src/Form/CompositorType.php
@@ -3,16 +3,20 @@
 namespace App\Form;
 
 use App\Entity\Compositor;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+
 
 class CompositorType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, [
+                'label' => 'Nom du compositeur',
+            ])
         ;
     }
 
