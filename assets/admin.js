bf33f2f25bf2617d8b1559c85f9925ef19eca71c
thibauttaglialatela <thibauttaglialatela@gmail.com>
2022-08-03 14:08:28 +0200

    ajout de datatables sur la page index Compositor

--- assets/admin.js
+++ assets/admin.js
@@ -10,3 +10,27 @@ import './styles/admin/admin.scss';
 
 // start the Stimulus application
 import './bootstrap';
+import 'datatables.net-bs5';
+
+$(document).ready(function () {
+    $('#datatables-index').DataTable({
+        pagingType: 'full_numbers',
+        search: {
+            return: true,
+        },
+        language: {
+            "search": "Rechercher:",
+            "emptyTable": "Aucune donnée disponible dans le tableau",
+            "info": "Affichage de _START_ à _END_ sur _TOTAL_ entrées",
+            "infoEmpty": "Affichage de 0 à 0 sur 0 entrées",
+            "infoFiltered": "(filtrées depuis un total de _MAX_ entrées)",
+            "lengthMenu": "Afficher _MENU_ entrées",
+            "paginate": {
+                "first": "Première",
+                "last": "Dernière",
+                "next": "&raquo;",
+                "previous": "&laquo;"
+            },
+        },
+    });
+});
