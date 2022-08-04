0b8bc99b270cf3a102dba15631b3e03f97b50363
thibauttaglialatela <thibauttaglialatela@gmail.com>
2022-08-04 14:59:43 +0200

    Formulaire de création et édition de musiciens OK

--- src/Entity/Instrument.php
+++ src/Entity/Instrument.php
@@ -7,10 +7,10 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: InstrumentRepository::class)]
 #[UniqueEntity('fct_id')]
+#[UniqueEntity('name')]
 class Instrument
 {
     #[ORM\Id]
@@ -18,7 +18,7 @@ class Instrument
     #[ORM\Column(type: 'integer')]
     private int $id;
 
-    #[ORM\Column(type: 'string', length: 255)]
+    #[ORM\Column(type: 'string', length: 255, unique: true)]
     private string $name;
 
     #[ORM\Column(type: 'string', length: 10, unique: true)]
--- src/Form/MusicianType.php
+++ src/Form/MusicianType.php
@@ -2,8 +2,12 @@
 
 namespace App\Form;
 
+use App\Entity\Instrument;
 use App\Entity\Musician;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +16,25 @@ class MusicianType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('lastname')
-            ->add('firstname')
-            ->add('status')
-            ->add('instruments')
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom de famille',
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom'
+            ])
+            ->add('status', ChoiceType::class, [
+                'choices' => [
+                    'Amateur' => 'Amateur',
+                    'Professionnel' => 'Professionnel',
+                ],
+                'label' => 'Statut',
+            ])
+            ->add('instruments', EntityType::class, [
+                'class' => Instrument::class,
+                'choice_label' => 'name',
+                'expanded' => true,
+                'multiple' => true,
+            ])
         ;
     }
 
