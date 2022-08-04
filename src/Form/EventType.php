eb0f84e5a02102f309427c807fc6d52fb7ee31a7
thibauttaglialatela <thibauttaglialatela@gmail.com>
2022-08-04 16:15:03 +0200

    ajout du HTML sanitizer

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -4,6 +4,11 @@ namespace App\Form;
 
 use App\Entity\Event;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,14 +17,45 @@ class EventType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('summary')
-            ->add('body')
-            ->add('category')
-            ->add('poster')
-            ->add('start_at')
-            ->add('end_at')
-            ->add('localisation')
+            ->add('title', TextType::class, [
+                'label' => 'Titre'
+            ])
+            ->add('summary', TextareaType::class, [
+                'label' => 'Résumé',
+                'attr' => [
+                    'col' => 10,
+                    'row' => 10
+                ],
+            ])
+            ->add('body', TextareaType::class, [
+                'label' => 'Contenu',
+                'attr' => [
+                    'col' => 10,
+                    'row' => 30
+                ],
+            ])
+            ->add('category', ChoiceType::class, [
+                'label' => 'Catégorie',
+                'choices' => [
+                    'placeholder' => false,
+                    'Concert' => 'Concert',
+                    'Répétition' => 'Répétition'
+                ],
+            ])
+            ->add('poster', UrlType::class, [
+                'label' => 'Illustration'
+            ])
+            ->add('start_at', DateTimeType::class, [
+                'date_label' => 'Débute à',
+                'date_widget' => 'single_text',
+            ])
+            ->add('end_at', DateTimeType::class, [
+                'date_label' => 'Se termine à',
+                'date_widget' => 'single_text',
+            ])
+            ->add('localisation', TextType::class, [
+                'label' => 'Lieu de l\'évenement'
+            ])
         ;
     }
 
