0da0a863674bf4fd2f77f6b7714f0f88c21fd13d
thibauttaglialatela <thibauttaglialatela@gmail.com>
2022-08-04 15:31:26 +0200

    Create event CRUD

---
+++ src/Form/EventType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Event;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('summary')
+            ->add('body')
+            ->add('category')
+            ->add('poster')
+            ->add('start_at')
+            ->add('end_at')
+            ->add('localisation')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Event::class,
+        ]);
+    }
+}
