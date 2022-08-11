6856e20b4f89f1b0b82e1842dbf689b97246b7c6
thibauttaglialatela <thibauttaglialatela@gmail.com>
2022-08-11 08:46:31 +0200

    MAJ webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const Encore = require('@symfony/webpack-encore');
 
-// Manually configure the runtime environment if not already configured yet by the "encore" command.
-// It's useful when you use tools that rely on webpack.config.js file.
 if (!Encore.isRuntimeEnvironmentConfigured()) {
     Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
@@ -9,11 +7,6 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
 Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
-
-
-// Uncomment the following line if you are using Webpack Encore <= 0.24
-// .addLoader({test: /\.json$/i, include: [require('path').resolve(__dirname, 'node_modules/ckeditor')], loader: 'raw-loader', type: 'javascript/auto'})
-
 
     .copyFiles({
         from: './assets/images',
